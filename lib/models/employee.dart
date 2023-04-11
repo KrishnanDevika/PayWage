baa54b3886568d213e57196e23546e828eae341b
Devika Krishnan <W0774427@myscc.ca>
2023-04-11 18:56:35 -0400

    Changed model class to fetch all employee profile details

--- lib/models/employee.dart
+++ lib/models/employee.dart
@@ -6,9 +6,30 @@ class Employee{
   int address;
   String contactNo;
   int wageType;
+  String city;
+  String state;
+  String street;
+  String occupation;
+  String salaryType;
+  int baseRate;
 
-  Employee({ required this.id,  required this.firstName, required this.lastName, required this.startDate, required this.address,
-      required this.contactNo, required this.wageType});
+
+  Employee({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.startDate,
+    required this.address,
+    required this.contactNo,
+    required this.wageType,
+    required this.city,
+    required this.state,
+    required this.street,
+    required this.occupation,
+    required this.salaryType,
+    required this.baseRate
+
+   });
 
   factory Employee.fromJson(Map<String, dynamic> parsedJson){
     return Employee(
@@ -19,6 +40,12 @@ class Employee{
       address: parsedJson['address'],
       contactNo: parsedJson['phone'],
       wageType: parsedJson['salary_type'],
+      city: parsedJson['city'],
+      state: parsedJson['state'],
+      street: parsedJson['street'],
+      occupation: parsedJson['occupation_type'],
+      salaryType : parsedJson['salary_type'],
+      baseRate: parsedJson['pay_rate']
 
     );
   }
