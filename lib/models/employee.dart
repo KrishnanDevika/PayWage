37984ccb2cb5cda3bd9787136e0829d3aeb3c40a
Devika Krishnan <W0774427@myscc.ca>
2023-04-11 22:11:14 -0400

    Modified employee model class removed unnecessary fields

--- lib/models/employee.dart
+++ lib/models/employee.dart
@@ -3,9 +3,7 @@ class Employee{
   String firstName;
   String lastName;
   String startDate;
-  int address;
   String contactNo;
-  int wageType;
   String city;
   String state;
   String street;
@@ -19,12 +17,10 @@ class Employee{
     required this.firstName,
     required this.lastName,
     required this.startDate,
-    required this.address,
     required this.contactNo,
-    required this.wageType,
+    required this.street,
     required this.city,
     required this.state,
-    required this.street,
     required this.occupation,
     required this.salaryType,
     required this.baseRate
@@ -37,12 +33,10 @@ class Employee{
       firstName: parsedJson['first_name'] ,
       lastName: parsedJson['last_name'],
       startDate: parsedJson['start_date'],
-      address: parsedJson['address'],
       contactNo: parsedJson['phone'],
-      wageType: parsedJson['salary_type'],
-      city: parsedJson['city'],
-      state: parsedJson['state'],
-      street: parsedJson['street'],
+        street: parsedJson['street'],
+        city: parsedJson['city'],
+        state: parsedJson['state'],
       occupation: parsedJson['occupation_type'],
       salaryType : parsedJson['salary_type'],
       baseRate: parsedJson['pay_rate']
